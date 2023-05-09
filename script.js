27dbaec2995c6cf80b2ad609ec58039c71d979d6
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-09 02:12:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,61 @@
-// Define quiz questions and answers
 const questions = [
 	{
 		question: "What is the capital of France?",
-		choices: ["Paris", "Berlin", "London", "Madrid"],
+		options: ["Paris", "London", "Berlin", "Madrid"],
 		answer: "Paris"
 	},
 	{
+		question: "What is the largest continent?",
+		options: ["Africa", "Asia", "Europe", "North America"],
+		answer: "Asia"
+	},
+	{
+		question: "What is the currency of Japan?",
+		options: ["Yuan", "Dollar", "Euro", "Yen"],
+		answer: "Yen"
+	},
+	{
 		question: "What is the tallest mammal?",
-		choices: ["Elephant", "Giraffe", "Horse", "Rhino"],
+		options: ["Elephant", "Giraffe", "Horse", "Lion"],
 		answer: "Giraffe"
 	},
 	{
 		question: "What is the largest planet in our solar system?",
-		choices: ["Mars", "Jupiter", "Earth", "Saturn"],
+		options: ["Venus", "Saturn", "Jupiter", "Neptune"],
 		answer: "Jupiter"
-	},
-	{
-		question: "What is the smallest country in the world?",
-		choices: ["China", "USA", "Vatican City", "Australia"],
-		answer: "Vatican City"
 	}
 ];
 
-// Get HTML elements
-const startBtn = document.getElementById("startBtn");
-const quiz = document.getElementById("quiz");
-const questionEl = document.getElementById("question");
-const choicesEl = document.getElementById("choices");
-const resultEl = document.getElementById("result");
-const timerEl = document.getElementById("timer");
+const container = document.querySelector(".container");
+const timer = document.querySelector("#timer");
+const startBtn = document.querySelector("#start");
+const questionDiv = document.querySelector("#question");
+const optionsDiv = document.querySelector("#options");
 
 let currentQuestion = 0;
+let score = 0;
 let time = 60;
-
-// Start quiz when start button is clicked
-startBtn.addEventListener("click", startQuiz);
+let timerInterval;
 
 function startQuiz() {
-	// Hide start button and show quiz
 	startBtn.style.display = "none";
-	quiz.style.display = "block";
-
-	// Start timer
-	let countdown = setInterval(function() {
-		time--;
-		timerEl.innerHTML = time;
-
-		if (time <= 0) {
-			clearInterval(countdown);
-			endQuiz();
-		}
-	}, 1000);
-
-	// Show first question
-	showQuestion();
+	timerInterval = setInterval(updateTimer, 1000);
+	updateQuestion();
 }
 
-function showQuestion() {
-	// Get current question
-	let q = questions[currentQuestion];
+function updateTimer() {
+	time--;
+	timer.textContent = time;
+	if (time === 0) {
+		endQuiz("Time is Up");
+	}
+}
 
-	// Update question and choices
-	questionEl.innerHTML = q.question;
-	choicesEl.innerHTML = "";
-
-	for (let i = 0; i < q.choices.length; i++) {
-		let choice = q.choices[i];
-		let btn = document.createElement("button");
-		btn.innerHTML = choice;
-		btn.addEventListener("click", function() {
-			if (choice === q.answer) {
-				currentQuestion++;
-				if (currentQuestion >= questions.length) {
-					endQuiz();
-				} else {
-					showQuestion();
-				}
-			} else {
-				time -= 10;
-				if (time < 0)
+function updateQuestion() {
+	questionDiv.textContent = questions[currentQuestion].question;
+	optionsDiv.innerHTML = "";
+	questions[currentQuestion].options.forEach(option => {
+		const optionDiv = document.createElement("div");
+		optionDiv.classList.add("option");
+		optionDiv.textContent = option;
+		optionDiv.addEventListener("click", () =>
